a7f9d7a66247742b39101af44b6d3160bcfe96d9
Sara Pereira <saragilberto@users.noreply.github.com>
2023-05-08 16:39:10 -0300

    CSS em Templates

--- src/template.js
+++ src/template.js
@@ -3,10 +3,19 @@ const selectTemplates = document.querySelectorAll('.template');
 
 const templates = document.querySelector('.templates');
 
-
+function templateSelection() {
+  for (let i = 0; i <selectTemplates.length; i++) {
+    selectTemplates[i].addEventListener("click", function() {
+      let currentTemplate = document.querySelectorAll("active-template");
+      currentTemplate[0].className = currentTemplate[0].className.replace("active-Template", ""
+      );
+      this.className += " active-template";
+    })
+  }
+}
 templates.addEventListener('click', (event) => {
   const id = event.target.dataset.imgId;
-  if (data-id) {
+  if (data-imgId) {
     // remove 'selected' class from other templates
     secTemplates.querySelectorAll('.template').forEach((template) => {
       template.classList.remove('selected');
