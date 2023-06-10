8267c8deb2ecdf48d7fddb7c66d75bd62e9587a0
Sara Gilberto <126068378+saragilberto@users.noreply.github.com>
2023-06-10 20:45:30 -0300

    final

--- src/app.js
+++ src/app.js
@@ -39,3 +39,20 @@ function PageTransitions() {
 
 PageTransitions();
 
+function createPDF() {
+
+  if(document.getElementById("nome").value == "" || document.getElementById("cidade").value == "") {
+      alert("Preencha todos os campos!");
+  }
+  else{
+      var doc = new jsPDF();
+
+      doc.text(document.getElementById("nome"), 10,10);
+      doc.text(document.getElementById("cidade"), 25,25);
+
+
+      doc.save(teste.pdf);
+  }
+
+}
+createPDF();
